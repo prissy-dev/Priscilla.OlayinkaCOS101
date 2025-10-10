0f1850ed133ee6dfdf144935c272e1cf441e66f8
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-10-10 15:05:17 +0100

    looking for bug

---
+++ week-2/practice_1.rs
@@ -0,0 +1,13 @@
+fn fn main() {
+	println!("Welcome to COS101!");
+
+	println("The course learning outcome is as follows");
+
+	println("1. Distinquish between com[uter and computer programming,/n
+			 2. Develop some techniques in computer science,/n
+			 3. Understand the different areas of study in computer science,/n
+			 4. Comnversant with the applications of computer science,/n
+			 5. Navigate the career prospects in computer science,/n
+			 6. Conversant with computer programming concepts,/n 
+		")
+}
