63cdbab0577c1aba6bc756cb652811de244200a0
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-10-11 09:04:55 +0100

    (C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-2\class-project.2rs.rs)

--- week-2/practice_1.rs
+++ week-2/practice_1.rs
@@ -1,7 +1,7 @@
-fn main() {
+fn main(){
 	println!("Welcome to COS 101!");
 
-	println!("The course learning outcome is as follows");
+	println!("The course learning outcome is as follows:");
 
 	println!("1. Distinquish between computer and computer programming,/n
 			 2. Develop some techniques in computer science,/n
--- week-2/practice_2.rs
+++ week-2/practice_2.rs
@@ -1,12 +1,10 @@
 fn main() {
-	println!("Welcome to COS 101!");
 
-	println!("The course learning outcome is as follows");
+	println!(); //prints just a
 
-	println!("1. Distinquish between computer and computer programming,/n
-			 2. Develop some techniques in computer science,/n
-			 3. Understand the different areas of study in computer science,/n
-			 4. Comnversant with the applications of computer science,/n
-			 5. Navigate the career prospects in computer science./n
-			 6. Conversant with computer programming concepts.");
+	println!("hello ");//prints
+
+	println!("format {} arguments", "some"); //prints format some arguments
+
+	println!("My name {}. I am the course rep of stream 1", "Dagogo William-Jumbo");
 }
