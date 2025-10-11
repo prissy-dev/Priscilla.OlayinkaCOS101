cade5efee98a14ce998ae4cb13f7767a780ec855
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-10-11 08:17:54 +0100

    (C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-2\class-project.rs)

---
+++ week-2/class-project.rs
@@ -0,0 +1,11 @@
+fn main() {
+	let p:f64 - 1000.0;
+	let r:f64 - 1.0;
+	let t:f64 - 2.0;
+
+	// simpl interest
+	let a = p * ( 1.0 + (r / 100.0)) * t;
+	println!("Amount is {}", si);
+	let si = a - p;
+	println!("Simple Interest is {}", si)
+}
