701cd5b16eca652d17dadb2df755f652eab88186
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-10-24 07:44:57 +0100

    (C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-4\project_1)

---
+++ week-4/project_1/target/debug/deps/project_1.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-4\project_1\target\debug\deps\project_1.d: src\main.rs
+
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-4\project_1\target\debug\deps\project_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/project_1/target/debug/project_2.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-4\project_2\target\debug\project_2.exe: C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-4\project_2\src\main.rs
