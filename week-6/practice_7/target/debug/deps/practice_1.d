817739f60b4df208d25bae89b0256c01cfd4f354
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-11-05 19:46:29 +0100

    (C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-6\practice_7)

---
+++ week-6/practice_7/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-6\practice_7\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-6\practice_7\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
