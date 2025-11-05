e37fccc91ac0b7c62589bb178f08f48fa7b4c8ec
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-11-05 18:27:40 +0100

    (C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-6\practice_2)

---
+++ week-6/practice_2/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+	let empty_string = String::new();
+	println!("lenght of empty_stringis {}",empty_string.len());
+
+	let content_string = String::from("Computer Science");
+	println!("lenght of content_string is {}",content_string.len());
+}
