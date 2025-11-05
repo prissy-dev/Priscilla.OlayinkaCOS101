bc2eb5b2b8fd812bedf7149e2fe3232175f9aea3
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-11-05 19:07:22 +0100

    (C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-6\practice_4)

---
+++ week-6/practice_4/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+	let fullname = "Chibudum John Umeh";
+	let department = "Computer Science";
+	let uni = "Pan-Atlantic University";
+
+	let mut school = "Schoolof Science".to_string();
+	school.push_str("and Technology");
+	println!("My name is {}",fullname);
+
+	
+	println!("The lenght my fullname is {}",fullname.len());
+	println!("I am a student of {} Department",department);
+	println!("{}",school);
+	println!("{}",uni);
+
+
+
+
+}
