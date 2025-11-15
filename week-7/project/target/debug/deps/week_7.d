9c5bead23ac731d382db77c01a1b5ff3728606c8
Priscilla <priscilla.olayinka@pau.edu.ng>
2025-11-15 18:44:14 +0100

    calculations

---
+++ week-7/project/target/debug/deps/week_7.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-7\project\target\debug\deps\week_7.d: src\main.rs
+
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-7\project\target\debug\deps\week_7.exe: src\main.rs
+
+src\main.rs:
