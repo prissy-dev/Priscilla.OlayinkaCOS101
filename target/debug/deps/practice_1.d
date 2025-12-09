96db67cb4aa4515df2da3e9e626e3ab5f4f48a06
prissy-dev <priscilla.olayinka@pau.edu.ng>
2025-12-09 18:50:47 +0100

    Delete target directory

--- target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ISMS\Documents\Priscilla.OlayinkaCOS101\week-10\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-C:\Users\ISMS\Documents\Priscilla.OlayinkaCOS101\week-10\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-src\main.rs:
