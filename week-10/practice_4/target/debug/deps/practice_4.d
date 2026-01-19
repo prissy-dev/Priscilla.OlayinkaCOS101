57a9e720ef62f7afc11663c9943e12193c4ad6b2
Priscilla <priscilla.olayinka@pau.edu.ng>
2026-01-19 07:02:21 +0100

    Listing of values

---
+++ week-10/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-10\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-10\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-10\practice_4\target\debug\practice_4.exe: C:\Users\New\Documents\Priscilla.OlayinkaCOS101\week-10\practice_4\src\main.rs
